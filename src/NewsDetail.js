2860b4c40ce4e4e2e5169802c6cdc3f69ea4257e
RuddyM22 <ruddyjosuemendoza@gmail.com>
2023-06-17 21:35:59 -0600

    Creando componente NewsDetail (paso 6)

---
+++ src/NewsDetail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
+const NewsDetail = ({ navigation }) => {
+    const route = useRoute();
+    const newsItem = route.params?.newsItem;
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{newsItem.title}</Text>
+            <Text style={styles.description}>{newsItem.description}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    description: {
+        fontSize: 14,
+    },
+});
+
+export default NewsDetail;
